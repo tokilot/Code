28a1371de10e4f7a45cac34a6f11ced890168c53
Tokinoshi <834765927@QQ.com>
2023-06-20 23:19:18 +0800

    complete Code/machine_learning/Learning/shopping by 2023/06/20

--- machine_learn/cs50ai-master/week4/shopping/shopping.py
+++ machine_learn/cs50ai-master/week4/shopping/shopping.py
@@ -10,13 +10,13 @@ TEST_SIZE = 0.4
 
 
 def main():
-
-    # Check command-line arguments
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python shopping.py data")
-
-    # Load data from spreadsheet and split into train and test sets
-    evidence, labels = load_data(sys.argv[1])
+    # # Check command-line arguments
+    # if len(sys.argv) != 2:
+    #     sys.exit("Usage: python shopping.py data")
+    #
+    # # Load data from spreadsheet and split into train and test sets
+    # evidence, labels = load_data(sys.argv[1])
+    evidence, labels = load_data("shopping.csv")
     X_train, X_test, y_train, y_test = train_test_split(
         evidence, labels, test_size=TEST_SIZE
     )
